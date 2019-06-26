ee97493bb56e3bdb313ea66eae1452d843ed9ab9
kishores4200 <kishores4200@gmail.com>
2019-06-26 14:00:20 -0700

    set up board with neighbors

--- tictactoe/main.py
+++ tictactoe/main.py
@@ -1,6 +1,31 @@
 from square import Square
+from enum import Enum, auto
 
-sq1 = Square(1)
-print(sq1.getValue())
-sq1.setValue("X")
-print(sq1.getValue())
+class AutoName(Enum):
+    def _generate_next_value_(name, start, count, last_values):
+        return name
+        
+class Direction(AutoName):
+    NORTH = auto()
+    SOUTH = auto()
+    EAST = auto()
+    WEST = auto()
+    NORTHEAST = auto()
+    NORTHWEST = auto()
+    SOUTHEAST = auto()
+    SOUTHWEST = auto()
+
+board = []
+for i in range(9):
+    board.append(Square(i))
+board[0].setNeighbors([[board[1], Direction.EAST], [board[3], Direction.SOUTH], [board[4], Direction.SOUTHEAST]])
+board[1].setNeighbors([[board[0], Direction.WEST], [board[2], Direction.EAST], [board[3], Direction.SOUTHWEST], [board[4], Direction.SOUTH], [board[5], Direction.SOUTHEAST]])
+board[2].setNeighbors([[board[1], Direction.WEST], [board[4], Direction.SOUTHWEST], [board[5], Direction.SOUTH]])
+board[3].setNeighbors([[board[0], Direction.NORTH], [board[1], Direction.NORTHEAST], [board[4], Direction.EAST], [board[7], Direction.SOUTHEAST], [board[6], Direction.SOUTH]])
+board[4].setNeighbors([[board[0], Direction.NORTHWEST], [board[1], Direction.NORTH], [board[2], Direction.NORTHEAST], [board[3], Direction.WEST], [board[5], Direction.EAST], [board[6], Direction.SOUTHWEST], [board[7], Direction.SOUTH], [board[8], Direction.SOUTHEAST]])
+board[5].setNeighbors([[board[4], Direction.WEST], [board[1], Direction.NORTHWEST], [board[2], Direction.NORTH], [board[8], Direction.SOUTH], [board[7], Direction.SOUTHWEST]])
+board[6].setNeighbors([[board[3], Direction.NORTH], [board[4], Direction.NORTHEAST], [board[7], Direction.EAST]])
+board[7].setNeighbors([[board[6], Direction.WEST], [board[3], Direction.NORTHWEST], [board[4], Direction.NORTH], [board[5], Direction.NORTHEAST], [board[8], Direction.EAST]])
+board[8].setNeighbors([[board[7], Direction.WEST], [board[4], Direction.NORTHWEST], [board[5], Direction.NORTH]])
+
+
--- tictactoe/square.py
+++ tictactoe/square.py
@@ -1,12 +1,26 @@
 class Square:
 
-    def __init__(self, value):
-        self.value = value
+    def __init__(self, location):
+        self.value = ""
+        self.location = location
 
     def setValue(self, newValue):
         self.value = newValue
 
     def getValue(self):
         return self.value
+
+    def setNeighbors(self, neighbors):
+        self.neighbors = neighbors
+
+    def getNeighbors(self):
+        return self.neighbors
+
+    def getMatchingNeighbors(self):
+        matchingNeighbors = []
+        for n in getNeighbors():
+            if n.getValue() == self.value:
+                matchingNeighbors.append(n)
+        return matchingNeighbors
 
     
