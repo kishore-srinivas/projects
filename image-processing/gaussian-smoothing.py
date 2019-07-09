2f8fbfc7d8761397d8fc031e478cdf5710b728ce
kishores4200 <kishores4200@gmail.com>
2019-07-08 18:57:30 -0700

    basic attempt at edge detection

--- image-processing/gaussian-smoothing.py
+++ image-processing/gaussian-smoothing.py
@@ -22,28 +22,38 @@ cv2.namedWindow('blur1', cv2.WINDOW_NORMAL)
 cv2.resizeWindow('blur1', 300, 250)
 cv2.imshow('blur1', blur1)
 
-blur3 = cv2.GaussianBlur(img, (111,111), 0)
-cv2.namedWindow('blur3', cv2.WINDOW_NORMAL)
-cv2.resizeWindow('blur3', 300, 250)
-cv2.imshow('blur3', blur3)
+gauss1 = cv2.GaussianBlur(img, (111,111), 0)
+cv2.namedWindow('gauss1', cv2.WINDOW_NORMAL)
+cv2.resizeWindow('gauss1', 300, 250)
+cv2.imshow('gauss1', gauss1)
+
+gauss2 = cv2.GaussianBlur(img, (71,71), 0)
+# cv2.namedWindow('gauss2', cv2.WINDOW_NORMAL)
+# cv2.resizeWindow('gauss2', 300, 250)
+# cv2.imshow('gauss2', gauss2)
+
+gauss3 = cv2.GaussianBlur(img, (31,31), 0)
+# cv2.namedWindow('gauss2', cv2.WINDOW_NORMAL)
+# cv2.resizeWindow('gauss2', 300, 250)
+# cv2.imshow('gauss2', gauss3)
 
 kernel2 = np.array([[0, -0.25, 0],
                     [-0.25, 2, -0.25],
                     [0, -0.25, 0]])
 blur2 = cv2.filter2D(img, -1, kernel2)
-cv2.namedWindow('blur2', cv2.WINDOW_NORMAL)
-cv2.resizeWindow('blur2', 300, 250)
-cv2.imshow('blur2', blur2)
+# cv2.namedWindow('blur2', cv2.WINDOW_NORMAL)
+# cv2.resizeWindow('blur2', 300, 250)
+# cv2.imshow('blur2', blur2)
 
-diff = blur2 - blur3
+diff = gauss1 - gauss2
 cv2.namedWindow('diff', cv2.WINDOW_NORMAL)
 cv2.resizeWindow('diff', 300, 250)
 cv2.imshow('diff', diff)
 
-diff2 = blur3 + diff
-cv2.namedWindow('diff2', cv2.WINDOW_NORMAL)
-cv2.resizeWindow('diff2', 300, 250)
-cv2.imshow('diff2', diff2)
+diff2 = gauss1 + diff
+# cv2.namedWindow('diff2', cv2.WINDOW_NORMAL)
+# cv2.resizeWindow('diff2', 300, 250)
+# cv2.imshow('diff2', diff2)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
