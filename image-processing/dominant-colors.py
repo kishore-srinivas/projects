d26c5747e1f4de968f38363b50aff6c184cdae66
kishores4200 <kishores4200@gmail.com>
2019-07-21 15:23:44 -0700

    shape and combine image and background

--- image-processing/dominant-colors.py
+++ image-processing/dominant-colors.py
@@ -15,21 +15,24 @@ _, labels, palette = cv2.kmeans(pixels, n_colors, None, criteria, 10, flags)
 _, counts = np.unique(labels, return_counts=True)
 dominant = palette[np.argmax(counts)]
 
-cv2.namedWindow('image', cv2.WINDOW_NORMAL)
-cv2.resizeWindow('image', 300, 250)
-cv2.imshow('image', img)
-
-color = np.zeros((300, 250, 3), np.uint8)
+imgShape = np.shape(img)
+scaleFactor = 1.25
+scaled = np.multiply(imgShape, (scaleFactor, scaleFactor, 1)).astype(int)
+color = np.zeros((scaled[0], scaled[1], scaled[2]), np.uint8)
 color[:] = (dominant[0], dominant[1], dominant[2])
-cv2.namedWindow('color', cv2.WINDOW_NORMAL)
-cv2.resizeWindow('color', 300, 250)
-cv2.imshow('color', color)
+# lo = np.amin(color, axis=2, keepdims=True)
+# hi = np.amax(color, axis=2, keepdims=True)
+# color = (lo + hi) - color #sets color to its complement
+minY = int((scaled[1]-imgShape[1])/2)
+maxY = int(scaled[1] - (scaled[1]-imgShape[1])/2)
+minX = int((scaled[0]-imgShape[0])/2)
+maxX = int(scaled[0] - (scaled[0]-imgShape[0])/2)
+for x in range(minX, maxX):
+    for y in range(minY, maxY):
+        color[x, y] = img[x-minX, y-minY]
 
-lo = np.amin(color, axis=2, keepdims=True)
-hi = np.amax(color, axis=2, keepdims=True)
-complement = (lo + hi) - color
-cv2.namedWindow('complement', cv2.WINDOW_NORMAL)
-cv2.resizeWindow('complement', 300, 250)
-cv2.imshow('complement', complement)
+cv2.namedWindow('result', cv2.WINDOW_NORMAL)
+cv2.resizeWindow('result', int(np.shape(color)[1]), int(np.shape(color)[0]))
+cv2.imshow('result', color)
 
 cv2.waitKey(0)
