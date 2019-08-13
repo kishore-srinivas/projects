c92f47b23c626ea7d1b6113db8a0b122eac396e8
kishores4200 <kishores4200@gmail.com>
2019-08-13 16:59:49 -0700

    updated sliders

--- image-processing/selective-coloration.py
+++ image-processing/selective-coloration.py
@@ -2,7 +2,6 @@ import numpy as np
 import cv2
 from tkinter import *
 import math
-import time
 
 ''' pseudocode:
 read in image in color and grayscale
@@ -13,40 +12,51 @@ define color clusters based on sensitivity
 classify pixel as a specific cluster
 '''
 
-original = cv2.imread('images\\abstract.jpg')
+original = cv2.imread('images\\lion.jpg')
 grayscale = cv2.cvtColor(original, cv2.COLOR_BGR2GRAY)
+loading = cv2.imread('images\\loading.jpg')
 
 '''
-calculates the euclidean distance between a pixel and a color with a specific bias
+calculates the euclidean distance between a pixel and a color with a specific weight
 @param pixel - the pixel to compare
 @param color - the BGR color to compare to
-@param bias - the weight to give to each color channel (a higher bias makes it harder to be classified as that color)
+@param weight - the weight to give to each color channel (a higher weight makes it harder to be classified as that color)
 '''
-def euclideanDistance(pixel, color, bias):
-    for i in range(len(bias)):
-        if (bias[i] < 0):
-            bias[i] = 0
-    deltaBlue = (pixel[0] - color[0]) * bias[0]
-    deltaGreen = (pixel[1] - color[1]) * bias[1]
-    deltaRed = (pixel[2] - color[2]) * bias[2]
+def euclideanDistance(pixel, color, weight):
+    for i in range(len(weight)):
+        if (weight[i] < 0):
+            weight[i] = 0
+    deltaBlue = (pixel[0] - color[0]) * weight[0]
+    deltaGreen = (pixel[1] - color[1]) * weight[1]
+    deltaRed = (pixel[2] - color[2]) * weight[2]
     return math.sqrt(deltaBlue ** 2 + deltaGreen ** 2 + deltaRed ** 2)
 
-def isColor(pixel, color, tolerance, bias=[1, 1, 1]):
-    distance = euclideanDistance(pixel, color, bias)
+def isColor(pixel, color, tolerance, weight=[1, 1, 1]):
+    distance = euclideanDistance(pixel, color, weight)
     return distance <= tolerance
 
-def update(color, biases):    
-    print(color, biases)
+def update(color, weights, tolerance): 
+    solid = np.zeros((10, 10, 3), np.uint8)   
+    solid[:] = (color[0], color[1], color[2])
+    # print(solid)
+    print(np.shape(solid))
+    cv2.namedWindow('color', cv2.WINDOW_NORMAL)
+    cv2.resizeWindow('color', 10, 10)
+    cv2.imshow('color', solid)   
+    # cv2.waitKey(0)
     combo = np.zeros_like(original)
-    for y in range(np.shape(original)[0]):
-        for x in range(np.shape(original)[1]):
-            if (isColor(original[y, x], color, 150, biases)):
+    rangeY = np.shape(original)[0]
+    rangeX = np.shape(original)[1]
+    for y in range(rangeY):
+        for x in range(rangeX):
+            if (isColor(original[y, x], color, tolerance, weights)):
                 combo[y, x] = original[y, x]
             else:
                 combo[y, x] = grayscale[y, x]
 
     result = combo
     cv2.imshow('result', result)
+    print("DONE")
     # cv2.waitKey(0)
 
 cv2.namedWindow('result', cv2.WINDOW_NORMAL)
@@ -54,21 +64,23 @@ cv2.imshow('result', original)
 # cv2.waitKey(0)
 
 def main():
-    update([w1.get(), w2.get(), w3.get()], [w4.get(), w5.get(), w6.get()])
+    update([c1.get(), c2.get(), c3.get()], [w1.get(), w2.get(), w3.get()], t.get())
 
 master = Tk()
-w1 = Scale(master, label='Blue', from_=0, to=255, resolution=1, orient=HORIZONTAL)
+c1 = Scale(master, label='Blue', from_=0, to=255, resolution=1, orient=HORIZONTAL)
+c1.pack()
+c2 = Scale(master, label='Green', from_=0, to=255, resolution=1, orient=HORIZONTAL)
+c2.pack()
+c3 = Scale(master, label='Red', from_=0, to=255, resolution=1, orient=HORIZONTAL)
+c3.pack()
+w1 = Scale(master, label='Blue weight', from_=0, to=15, resolution=0.25, orient=HORIZONTAL)
 w1.pack()
-w2 = Scale(master, label='Green', from_=0, to=255, resolution=1, orient=HORIZONTAL)
+w2 = Scale(master, label='Green weight', from_=0, to=15, resolution=0.25, orient=HORIZONTAL)
 w2.pack()
-w3 = Scale(master, label='Red', from_=0, to=255, resolution=1, orient=HORIZONTAL)
+w3 = Scale(master, label='Red weight', from_=0, to=15, resolution=0.25, orient=HORIZONTAL)
 w3.pack()
-w4 = Scale(master, label='Blue bias', from_=0, to=15, resolution=0.25, orient=HORIZONTAL)
-w4.pack()
-w5 = Scale(master, label='Green bias', from_=0, to=15, resolution=0.25, orient=HORIZONTAL)
-w5.pack()
-w6 = Scale(master, label='Red bias', from_=0, to=15, resolution=0.25, orient=HORIZONTAL)
-w6.pack()
+t = Scale(master, label='Tolerance', from_=0, to=255, resolution=1, orient=HORIZONTAL)
+t.pack()
 Button(master, text='Update', command=main).pack()
 
 mainloop()
