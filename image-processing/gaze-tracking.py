17ca362ffea209b27b9a2fd4bd56bb7e0cf41fc6
kishores4200 <kishores4200@gmail.com>
2019-08-20 13:51:57 -0700

    tracks iris position
    needs work on reliability to make sure circles are always detected

---
+++ image-processing/gaze-tracking.py
@@ -0,0 +1,111 @@
+import cv2
+import numpy as np
+cv2DataPath = 'C:\\Program Files\\Python\\Python37-32\\Lib\\site-packages\\cv2\\data\\'
+
+# original = cv2.imread('images\\timberlake.jpg')
+# grayscale = cv2.cvtColor(original, cv2.COLOR_BGR2GRAY)
+
+face_cascade = cv2.CascadeClassifier(cv2DataPath + 'haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier(cv2DataPath + 'haarcascade_eye.xml')
+
+def detect_face(img):
+    gray_frame = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    coords = face_cascade.detectMultiScale(gray_frame, 1.3, 5)
+    if len(coords) > 1:
+        biggest = (0, 0, 0, 0)
+        for i in coords:
+            if i[3] > biggest[3]:
+                biggest = i
+        biggest = np.array([i], np.int32)
+    elif len(coords) == 1:
+        biggest = coords
+    else:
+        return None
+    for (x, y, w, h) in biggest:
+        return np.array([x, y, w, h])
+
+def detect_eyes(img):
+    gray_frame = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    eyes = eye_cascade.detectMultiScale(gray_frame, 1.05, 5) # detect eyes
+    width = np.size(img, 1) # get face frame width
+    height = np.size(img, 0) # get face frame height
+    left_eye = None
+    right_eye = None
+    for (x, y, w, h) in eyes:
+        if y > height / 2:
+            pass
+        eyecenter = x + w / 2  # get the eye center
+        if eyecenter < width * 0.5:
+            # left_eye = img[y:y + h, x:x + w]
+            right_eye = np.array([x, y, w, h])
+        else:
+            # right_eye = img[y:y + h, x:x + w]
+            left_eye = np.array([x, y, w, h])
+    return left_eye, right_eye
+
+def detect_iris(img):
+    gray_eyes = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    _, adjusted = cv2.threshold(gray_eyes, 100, 255, cv2.THRESH_BINARY)
+    adjusted = cv2.GaussianBlur(adjusted, (7, 7), 1)
+    # adjusted = cv2.blur(adjusted, (5, 5))
+    height, width = np.shape(adjusted)
+    # adjusted = adjusted[int(.2*height):height]
+    adjusted[0:int(.3*height), 0:width] = 255
+
+    circles = cv2.HoughCircles(adjusted,cv2.HOUGH_GRADIENT,1,20,param1=30,param2=10,maxRadius=int(height/2))
+    circles = np.uint16(np.around(circles))
+    adjusted = cv2.cvtColor(adjusted, cv2.COLOR_GRAY2BGR)
+    for i in circles[0,:]:
+        # draw the outer circle
+        # cv2.circle(adjusted,(i[0],i[1]),i[2],(0,255,0),2)
+        # draw the center of the circle
+        cv2.circle(adjusted,(i[0],i[1]),1,(0,0,255),3)
+        center = (i[0],i[1])
+
+    cv2.namedWindow('adjusted', cv2.WINDOW_NORMAL)
+    cv2.resizeWindow('adjusted', 100, 100)
+    cv2.imshow('adjusted', adjusted)
+
+    # return minLoc
+    return center
+
+cap = cv2.VideoCapture(0)
+if not cap.isOpened():
+    raise IOError("Cannot open webcam")
+
+while(True):
+    _, original = cap.read()
+    original = cv2.flip(original, 1)
+    result = np.copy(original)
+
+    try:
+        x, y, w, h = detect_face(original)
+    except TypeError:
+        x = 0
+        y = 0
+        w = np.size(original, 1)
+        h = np.size(original, 0)
+    face = original[y:y+h, x:x+w]
+    cv2.rectangle(result, (x, y), (x+w, y+h), (255, 255, 0), 2)
+
+    try:
+        [lx, ly, lw, lh], [rx, ry, rw, rh] = detect_eyes(face)
+    except TypeError:
+        lx = ly = rx = ry = 0
+        lw = rw = int(w/2)
+        lh = rh = int(h/2)
+    leftEye = face[ly:ly+lh, lx:lx+lw]
+    rightEye = face[ry:ry+rh, rx:rx+rw]
+    cv2.rectangle(result, (x + lx, y + ly), (x + lx+lw, y + ly+lh), (0, 255, 255), 2)
+    cv2.rectangle(result, (x + rx, y + ry), (x + rx+rw, y + ry+rh), (0, 255, 255), 2)
+
+    cv2.circle(result, tuple(map(lambda x, y, z: x + y + z, (x, y), (lx, ly), detect_iris(leftEye))), 1, (0, 0, 255), 3)
+    cv2.circle(result, tuple(map(lambda x, y, z: x + y + z, (x, y), (rx, ry), detect_iris(rightEye))), 1, (0, 0, 255), 3)
+
+    cv2.namedWindow('result', cv2.WINDOW_NORMAL)
+    # cv2.resizeWindow('result', int(np.shape(original)[1]/2), int(np.shape(original)[0]/2))
+    cv2.imshow('result', result)
+    if cv2.waitKey(1) == 27: 
+            break  # esc to quit
+cv2.destroyAllWindows()
+# cv2.waitKey(0)
