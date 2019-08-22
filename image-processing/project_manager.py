3c01aeb29c36b6dd1e6e6c07329368bdc4717864
kishores4200 <kishores4200@gmail.com>
2019-08-22 13:12:04 -0700

    project manager to allow easy IO

---
+++ image-processing/project_manager.py
@@ -0,0 +1,38 @@
+import cv2
+from enum import Enum
+
+import dominant_colors
+import east_text_detection
+import gaussian_smoothing
+import gaze_tracking
+import lane_detection
+import math_tests
+import selective_coloration
+import text_extraction
+
+class InputType(Enum):
+    IMAGE = 0
+    VIDEO = 1
+
+''' change these to match the program you wish to call '''
+inputType = InputType.VIDEO
+source = 'images\\test2.mp4'
+PROGRAM = lane_detection
+
+try:
+    if (inputType == InputType.IMAGE):
+        img = cv2.imread(source)
+        PROGRAM.main(img)
+    elif (inputType == InputType.VIDEO):
+        cap = cv2.VideoCapture(source)
+        if not cap.isOpened():
+            raise IOError("Cannot open video source")
+        while(True):
+            _, img = cap.read()
+            PROGRAM.main(img)
+            if (cv2.waitKey(1) == 27):
+                break
+        cap.release()
+        cv2.destroyAllWindows()
+except AttributeError:
+    print('input type and source do not match, quitting')
