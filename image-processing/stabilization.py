98b6b9cfa56aae06f2e48739a9f0a9c1a7e29b5c
kishores4200 <kishores4200@gmail.com>
2019-08-28 22:56:30 -0700

    click and crop ROI to stabilize around

--- image-processing/stabilization.py
+++ image-processing/stabilization.py
@@ -8,30 +8,49 @@ minY = 80
 maxY = 155
 minX = 440
 maxX = 515
+startCorner = None
+stopCorner = None
+frame = None
+copy = None
+
+def crop(event, x, y, flags, param):
+    global startCorner, stopCorner, roi, copy
+    if (startCorner != None):
+        frame = np.copy(copy)
+        cv2.rectangle(frame, startCorner, (x,y), (0,255,0), 2)
+        cv2.imshow('frame', frame)
+    if (event == cv2.EVENT_LBUTTONDOWN):
+        startCorner = (x, y)
+    elif (event == cv2.EVENT_LBUTTONUP):
+        stopCorner = (x, y)
+        print(startCorner, stopCorner)
+        # cv2.rectangle(frame, startCorner, stopCorner, (0,0,255), 2)
+
 def __init__(source):
-    # TODO add cropping to set roi
-    global roi, minY, maxY, minX, maxX
+    global roi, minY, maxY, minX, maxX, startCorner, stopCorner, frame, copy
     print("init")
     vidcap = cv2.VideoCapture(source)
     success, frame = vidcap.read()
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    roi = frame[minY:maxY, minX:maxX]
-    cv2.rectangle(frame, (minX,minY), (maxX,maxY), (0,0,255))
-    if success:
-        # cv2.imshow('frame', frame)
-        cv2.imshow('roi', roi)  
-        # cv2.waitKey(0)
+    copy = np.copy(frame)
+    cv2.imshow('frame', frame)
+    cv2.setMouseCallback('frame', crop)
+    if cv2.waitKey(0) == ord("c"):
+        print('c pressed')
+        print(startCorner, stopCorner)
+        if (startCorner != None) and (stopCorner != None):
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            roi = frame[startCorner[1]:stopCorner[1], startCorner[0]:stopCorner[0]]
+            cv2.imshow("ROI", roi)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
 
 def main(img):
+    cv2.imshow('img', img)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     res = cv2.matchTemplate(gray, roi, cv2.TM_SQDIFF_NORMED)
     min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
     corner = min_loc
-    # cv2.rectangle(img, corner, (corner[0] + np.shape(roi)[1], corner[1] + np.shape(roi)[0]), (0, 0, 255))
     (dx, dy) = tuple(map(lambda x, y: x - y, corner, (minX, minY)))
-    # print(corner, (dx, dy))
-    # if (cv2.waitKey(0) == 27):
-    #     sys.exit()
     img = np.roll(img, -1 * dx, axis=1)
     img = np.roll(img, -1 * dy, axis=0)
     for i in range(0, dy, -1 if dy < 0 else 1):
