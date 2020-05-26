d06512615a7f1236337e71f695ad1dd50ac0c8b1
kishore-srinivas <kishores4200@gmail.com>
2020-05-25 17:29:06 -0700

    angled bouncing works

--- motion/balls.py
+++ motion/balls.py
@@ -131,14 +131,14 @@ lines = []
 # lines.append(Line([-FIELD_SIDE_LENGTH/2, 50], [0, 50]))
 # lines.append(Line([0, 30], [FIELD_SIDE_LENGTH/2, 30]))
 # angled lines
-# lines.append(Line([-FIELD_SIDE_LENGTH/2, 70], [FIELD_SIDE_LENGTH/8, 50]))
-# lines.append(Line([-FIELD_SIDE_LENGTH/8, 10], [FIELD_SIDE_LENGTH/2, 30]))
+lines.append(Line([-FIELD_SIDE_LENGTH/2, 70], [FIELD_SIDE_LENGTH/8, 50]))
+lines.append(Line([-FIELD_SIDE_LENGTH/8, 10], [FIELD_SIDE_LENGTH/2, 30]))
 # 45deg lines
 # lines.append(Line([-30, 70], [10, 30]))
 # lines.append(Line([40, 50], [0, 10]))
 # 30deg lines
-lines.append(Line([10-20*math.sqrt(3), 70], [10, 50]))
-lines.append(Line([40, 50], [0, 10]))
+# lines.append(Line([10-20*math.sqrt(3), 70], [10, 50]))
+# lines.append(Line([40, 50], [-20, -10]))
 
 # lines.append([0, FIELD_SIDE_LENGTH/2, 10, 50])
 
@@ -163,36 +163,30 @@ def animate(i):
         b = balls[i]
         yPos = b.getPosition()[1]
         acc = np.array([0, GRAVITY])
+        force = acc * b.getMass()
+
+        vel = np.linalg.norm(b.getVelocity())
+        print(b.getVelocity(), vel)
 
         for l in lines:
             if (l.isPointOnLine(b.getPosition())):
-                #TODO: implement angleOfIncidence
                 angleOfNormal = l.getHeading() + math.pi/2
-                # print(b.getVelocityDirection(), l.getHeading(), angleOfIncidence)
-                # angleOfNormal = math.pi/2
+                                
+                vfx = vel * math.cos(angleOfNormal)
+                vfy = vel * math.sin(angleOfNormal)
+                impulse = b.getMass() * (np.array([vfx, vfy]) - b.getVelocity())
+                force = (impulse / T) * b.getElasticity()
 
-                vel = np.linalg.norm(b.getVelocity())
-                print(b.getVelocity(), vel)
-                # impulse = -1 * b.getMass() * 2 * b.getVelocity() * np.array([math.sin(angleOfIncidence), math.cos(angleOfIncidence)])
-                # force = (impulse / T) * b.getElasticity()
-                # acc = force / b.getMass()
-                impulseX = b.getMass() * 2 * vel * math.cos(angleOfNormal) #* (-1 if b.getVelocity()[0] > 0 else 1)
-                forceX = (impulseX / T) * b.getElasticity()
-                impulseY = b.getMass() * 2 * vel * math.sin(angleOfNormal) #* (-1 if b.getVelocity()[1] > 0 else 1)
-                forceY = (impulseY / T) * b.getElasticity()
-                impulse = np.array([impulseX, impulseY])
-                # print(math.sin(angleOfIncidence), impulseX, math.cos(angleOfIncidence), impulseY)
-                acc = np.array([forceX, forceY] / b.getMass())
-                if (abs(acc[1] + GRAVITY) < 0.00001):
-                    acc[1] = 0
-                print(angleOfNormal, impulse, acc)
+                if (abs(force[1] + GRAVITY) < 0.00001):
+                    force[1] = 0
+                print(angleOfNormal, impulse, force)
 
-                particles.append(ax.arrow(*b.getPosition(), *acc, 
+                particles.append(ax.arrow(*b.getPosition(), *force, 
                     shape='full', head_starts_at_zero=True, width=1, ec="white", fc="red"))
 
-        b.move(acc * b.getMass())
+        b.move(force)
         particles.append(ax.plot(*b.getPosition(), 'bo', ms=b.getRadius()/2)[0])
-        particles.append(ax.arrow(*b.getPosition(), 2*math.cos(b.getVelocityDirection()), 2*math.sin(b.getVelocityDirection()), 
+        particles.append(ax.arrow(*b.getPosition(), vel*math.cos(b.getVelocityDirection()), vel*math.sin(b.getVelocityDirection()), 
             shape='full', head_starts_at_zero=True, width=1, ec="white"))
 
     return particles
