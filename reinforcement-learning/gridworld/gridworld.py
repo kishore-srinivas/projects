b7018dcd603dfd38e32c1f17a51d1153d0d8c82b
kishore-srinivas <kishores4200@gmail.com>
2020-07-16 20:28:03 -0700

    small change

--- reinforcement-learning/gridworld/gridworld.py
+++ reinforcement-learning/gridworld/gridworld.py
@@ -17,16 +17,18 @@ for k in qValues.keys():
     print(k)
     for k2 in qValues[k].keys():
         print("{}\t{}".format(k2, qValues[k][k2]))
-g.draw()
+qa.reset()
 
-# print()
-# reward = qa.grid.giveReward(qa.getPosition())
-# while (reward != 1):
-#     print(qa.getPosition())
-#     action = qa.chooseAction(exp=0)
-#     print(action)
-#     qa.takeAction(action)
-#     reward = qa.grid.giveReward(qa.getPosition())
-#     g.draw()
-# print(qa.getPosition(), 'GOAL')
-
+print()
+reward = qa.grid.giveReward(qa.getPosition())
+count = 0
+while (reward != 1):
+    print(qa.getPosition())
+    action = qa.chooseAction(exp=0)
+    print(action)
+    qa.takeAction(action)
+    reward = qa.grid.giveReward(qa.getPosition())
+    g.draw()
+    count += 1
+print(qa.getPosition(), 'GOAL')
+print(count, 'steps')
