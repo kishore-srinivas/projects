c2d882a3980263efa2ec3aa9f7ad9f7e14b00c56
kishore-srinivas <kishores4200@gmail.com>
2020-12-17 12:55:01 -0800

    matrices and neural network skeleton

---
+++ matrix-scanner-solver/model.py
@@ -0,0 +1,13 @@
+class NeuralNetwork:
+    numInputs = 0
+    numOutputs = 0
+    numHiddenLayers = 0
+
+    def NeuralNetwork():
+        pass
+
+    def train():
+        pass
+
+    def predict(image_fragment):
+        pass
