9bd9cbd6d381f8d1ef7398756d3467f12ac9f423
Kishore <kishores4200@gmail.com>
2021-03-26 16:23:58 -0700

    plotting paths at end with funcanimation

--- motion/ant-pathfinding.py
+++ motion/ant-pathfinding.py
@@ -58,33 +58,40 @@ class Ant:
     def getMeals(self):
         return self.meals
 
-NUM_ANTS = 3
+NUM_ANTS = 5
 NUM_FOOD = 15
 FIELD_SIZE = 100
 
 # create figure
 fig = plt.figure(figsize=(6, 5))
 fig.subplots_adjust(left=0, right=1, bottom=0.05, top=0.95)
-ax = fig.add_subplot(111, aspect='equal', autoscale_on=False, xlim=(0, FIELD_SIZE*1.5), ylim=(0, FIELD_SIZE*1.5))
+ax = fig.add_subplot(111, aspect='equal', autoscale_on=False, xlim=(0, FIELD_SIZE), ylim=(0, FIELD_SIZE))
+ax.grid()
 
 # place ants and foods randomly on the field
 ants = []
 for i in range(NUM_ANTS):
     randPos = np.array([np.random.randint(0, FIELD_SIZE), np.random.randint(FIELD_SIZE)])
     ants.append(Ant(randPos, i))
+    ax.plot(*randPos, 'gs')
 foods = []
 for i in range(NUM_FOOD):
     randPos = np.array([np.random.randint(0, FIELD_SIZE), np.random.randint(FIELD_SIZE)])
     foods.append(Food(randPos, i))
 eatenFoods = []
 
+# init function
+def init():
+    members = []
+    return members
+
 antsReturnedHome = 0
-while (antsReturnedHome < len(ants)):
-    ax.grid()
+# animate function, called repeatedly
+def animate(i):
+    global antsReturnedHome
+    members = []
 
     for a in ants:
-        ax.plot(*a.getStartPos(), 'gs')
-
         # find the closest food
         closestFood = None
         minDist = np.inf
@@ -116,26 +123,37 @@ while (antsReturnedHome < len(ants)):
             eatenFoods.append(closestFood)
 
         # plot the ant
-        ax.arrow(*a.getPos(), np.cos(a.getHeading()), np.sin(a.getHeading()), 
-            shape='full', head_starts_at_zero=True, width=1, ec="white", fc="red")
+        members.append(ax.arrow(*a.getPos(), np.cos(a.getHeading()), np.sin(a.getHeading()), 
+            shape='full', head_starts_at_zero=True, width=1, ec="white", fc="red"))
 
     # plot the foods
     for f in foods:
-        ax.plot(*f.getPos(), 'bo')
+        members.append(ax.plot(*f.getPos(), 'bo')[0])
     for ef in eatenFoods:
-        ax.plot(*ef.getPos(), 'kx')
-    plt.pause(0.000000001)
-    plt.cla()
+        members.append(ax.plot(*ef.getPos(), 'kx')[0])
 
-for a in ants:
-    ax.plot(*a.getStartPos(), 'gs')
-    ax.plot([a.getStartPos()[0], a.getMeals()[0].getPos()[0]], [a.getStartPos()[1], a.getMeals()[0].getPos()[1]], 'r-', lw=1)
-    for i in range(1, len(a.getMeals())):
-        ax.plot([a.getMeals()[i-1].getPos()[0], a.getMeals()[i].getPos()[0]], [a.getMeals()[i-1].getPos()[1], a.getMeals()[i].getPos()[1]], 'r-', lw=1)
-    ax.plot([a.getMeals()[-1].getPos()[0], a.getStartPos()[0]], [a.getMeals()[-1].getPos()[1], a.getStartPos()[1]], 'r-', lw=1)
+    # if all ants are home, plot the paths they took
+    if (antsReturnedHome == len(ants)):
+        print('all home')
+        print('new figure')
+        fig2 = plt.figure(figsize=(6, 5))
+        fig2.subplots_adjust(left=0, right=1, bottom=0.05, top=0.95)
+        ax2 = fig2.add_subplot(111, aspect='equal', autoscale_on=False, xlim=(0, FIELD_SIZE), ylim=(0, FIELD_SIZE))
 
-for ef in eatenFoods:
-    ax.plot(*ef.getPos(), 'kx')
-ax.grid()
+        for a in ants:
+            ax2.plot(*a.getStartPos(), 'gs')
+            ax2.plot([a.getStartPos()[0], a.getMeals()[0].getPos()[0]], [a.getStartPos()[1], a.getMeals()[0].getPos()[1]], '-', lw=1)
+            for i in range(1, len(a.getMeals())):
+                ax2.plot([a.getMeals()[i-1].getPos()[0], a.getMeals()[i].getPos()[0]], [a.getMeals()[i-1].getPos()[1], a.getMeals()[i].getPos()[1]], '-', lw=1)
+            ax2.plot([a.getMeals()[-1].getPos()[0], a.getStartPos()[0]], [a.getMeals()[-1].getPos()[1], a.getStartPos()[1]], '-', lw=1)
 
+        for ef in eatenFoods:
+            ax2.plot(*ef.getPos(), 'kx')
+        ax2.grid()
+        plt.show()
+        return []
+
+    return members
+
+ani = animation.FuncAnimation(fig, animate, frames=60, interval=5, blit=True, init_func=init)
 plt.show()
